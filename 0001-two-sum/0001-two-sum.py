e25903e8402a895b252ee5916a806b4421a9aa88
Visrut Jajadiya <120151957+Jajadiyavisrut@users.noreply.github.com>
2026-07-11 16:09:13 +0530

    Time: 0 ms (100%), Space: 12.8 MB (94.78%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,13 +1,9 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        ans=[]
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if i==j:
-                    pass
-                else:
-                    if nums[i]+nums[j]==target:
-                        ans.extend([i,j])
-                        return ans
-        return ans
-        
+        dic={}
+        for i,n in enumerate(nums):
+            dif=target-n
+            if dif in dic:
+                return [dic[dif],i]
+            dic[n]=i
+        return 
