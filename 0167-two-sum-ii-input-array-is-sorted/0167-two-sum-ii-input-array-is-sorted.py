28901bc979d4c03bd18703bfef77581f556f9240
Visrut Jajadiya <120151957+Jajadiyavisrut@users.noreply.github.com>
2026-07-12 10:12:31 +0530

    Time: 8 ms (28.86%), Space: 12.9 MB (12.62%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,8 +1,20 @@
 class Solution(object):
     def twoSum(self, numbers, target):
-        dic={}
-        for i,n in enumerate(numbers):
-            dif=target-n
-            if dif in dic:
-                return [dic[dif]+1,i+1]
-            dic[n]=i        
+      # solution 1
+        # dic={}
+        # for i,n in enumerate(numbers):
+        #     dif=target-n
+        #     if dif in dic:
+        #         return [dic[dif]+1,i+1]
+        #     dic[n]=i     
+
+        # solution 2 using two pointer
+        l,r=0,len(numbers)-1
+        while l<r:
+            ans=numbers[l]+numbers[r]
+            if ans>target:
+                r=r-1
+            elif ans<target:
+                l=l+1 
+            else:
+                return [l+1,r+1]
