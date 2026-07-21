625bdcd614c9f8cb3107080627ada0f29d9e750c
Visrut Jajadiya <120151957+Jajadiyavisrut@users.noreply.github.com>
2026-07-21 12:55:51 +0530

    Time: 7 ms (26.54%), Space: 14.9 MB (9.55%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.py
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -4,13 +4,17 @@ class Solution(object):
             return nums
         
         dic={}
+       #it check if item is in  dict then  add 1 other than if not in dict then assum 0 and add 1 
+        for num in nums:
+            dic[num] = dic.get(num, 0) + 1
+#it iterate through dic and only return which has value =1
+        return [k for k,v in dic.items() if v==1]
+       
+       
         # s,e=0,len(nums)-1
         # while s<e:
         #     dic[nums[s]]=+1
         #     dic[nums[e]]=+1
         #     s+=1
         #     e+=1
-        for num in nums:
-            dic[num] = dic.get(num, 0) + 1
-        return [k for k,v in dic.items() if v==1]
         # return dic
