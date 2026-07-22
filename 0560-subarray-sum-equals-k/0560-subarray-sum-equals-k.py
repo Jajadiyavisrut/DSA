f8fd6748b0e7c16c1cf08ac8bb3ea603fa73ce65
Visrut Jajadiya <120151957+Jajadiyavisrut@users.noreply.github.com>
2026-07-22 22:07:36 +0530

    Time: 39 ms (51.09%), Space: 15.2 MB (17.12%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,12 +1,30 @@
+# class Solution(object):
+#     def subarraySum(self, nums, k):
+#         ans=0
+#         curr=0
+#         pre={0:1}
+#         for  n in nums:
+#             curr+=n
+#             diff=curr-k
+#             ans+=pre.get(diff,0)
+#             pre[curr]=1+pre.get(curr,0)
+#         return ans
+        
 class Solution(object):
     def subarraySum(self, nums, k):
-        ans=0
-        curr=0
-        pre={0:1}
-        for  n in nums:
-            curr+=n
-            diff=curr-k
-            ans+=pre.get(diff,0)
-            pre[curr]=1+pre.get(curr,0)
-        return ans
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        count = 0
+        sums = 0
+        d = dict()
+        d[0] = 1
         
+        for i in range(len(nums)):
+            sums += nums[i]
+            count += d.get(sums-k,0)
+            d[sums] = d.get(sums,0) + 1
+        
+        return(count)
